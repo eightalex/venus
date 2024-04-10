db0d5d38376f3603a6920123e2c6e5b19033d724
eightalex <eight.alex@gmail.com>
2024-04-10 12:57:22 +0300

    Update submenu

--- functions.php
+++ functions.php
@@ -7,11 +7,11 @@ function filter_menu_item_classes( $classes ) {
 
 add_filter( 'nav_menu_css_class', 'filter_menu_item_classes', 10, 4 );
 
-//function filter_submenu_classes( $classes, $args ) {
-//    if ( $args->theme_location === 'main-menu' ) {
-//        $classes[] = 'navigation__submenu';
-//    }
-//    return $classes;
-//}
-//
-//add_filter( 'nav_menu_submenu_css_class', 'filter_submenu_classes', 10, 3 );
+function filter_submenu_classes( $classes, $args ) {
+    if ( $args->theme_location === 'main-menu' ) {
+        $classes[] = 'navigation__submenu';
+    }
+    return $classes;
+}
+
+add_filter( 'nav_menu_submenu_css_class', 'filter_submenu_classes', 10, 3 );
