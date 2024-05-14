8def5efff07e10ffaaf6da016e12193ad4ca29f6
eightalex <eight.alex@gmail.com>
2024-05-14 23:13:23 +0300

    Added slider block

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,10 @@
-
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        navigation: {
+            nextEl: '.js-button-next',
+            prevEl: '.js-button-prev',
+            disabledClass: 'disabled',
+        },
+    });
+});
