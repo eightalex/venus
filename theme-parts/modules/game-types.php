a044111ea61c719168d20ea3cbd0e64a13156dca
eightalex <eight.alex@gmail.com>
2024-06-16 14:11:30 +0300

    Added header to game types

--- theme-parts/modules/game-types.php
+++ theme-parts/modules/game-types.php
@@ -13,47 +13,61 @@ $section_subtitle   = $content['game_types_subtitle'];
 <section class="section section_suits section_img">
     <div class="container">
         <div class="section__inner">
-            <div class="game-types">
-                <?php
-                foreach($games as $game):
-                    $icon_id    = $game['gt_icon'];
-                    $icon_data  = apply_filters('ud_get_file_data', $icon_id);
-                    $title      = $game['gt_title'];
-                    $desc       = $game['gt_desc'];
-                    ?>
-                    <div class="game-types__item">
-                        <div class="game-types__header">
-                            <?php
-                            if(!empty($icon_data)):
-                                ?>
-                                <div class="game-types__icon">
-                                    <img src="<?php echo $icon_data['src']?>" alt="<?php echo $icon_data['alt']?>">
-                                </div>
+            <header class="section__header">
+                <div class="section__title">
+                    Secrets of slot machines - <em>top 3 strategies</em>
+                </div>
+                <div class="section__subtitle">
+                    Cras in nisi id turpis cursus vulputate.
+                    Aliquam at sapien non tellus congue efficitur.
+                    Nam scelerisque quam quis turpis pellentesque,
+                    in placerat erat laoreet. Integer porttitor
+                    malesuada ante, nec aliquet dui pellentesque sit amet.
+                </div>
+            </header>
+            <div class="section__content">
+                <div class="game-types">
+                    <?php
+                    foreach($games as $game):
+                        $icon_id    = $game['gt_icon'];
+                        $icon_data  = apply_filters('ud_get_file_data', $icon_id);
+                        $title      = $game['gt_title'];
+                        $desc       = $game['gt_desc'];
+                        ?>
+                        <div class="game-types__item">
+                            <div class="game-types__header">
                                 <?php
-                            endif;
+                                if(!empty($icon_data)):
+                                    ?>
+                                    <div class="game-types__icon">
+                                        <img src="<?php echo $icon_data['src']?>" alt="<?php echo $icon_data['alt']?>">
+                                    </div>
+                                <?php
+                                endif;
 
-                            if(!empty($title)):
-                                ?>
-                                <div class="game-types__title">
-                                    <?php echo $title?>
-                                </div>
+                                if(!empty($title)):
+                                    ?>
+                                    <div class="game-types__title">
+                                        <?php echo $title?>
+                                    </div>
                                 <?php
+                                endif;
+                                ?>
+                            </div>
+                            <?php
+                            if(!empty($desc)):
+                                ?>
+                                <div class="game-types__text">
+                                    <?php echo $desc?>
+                                </div>
+                            <?php
                             endif;
                             ?>
                         </div>
-                        <?php
-                        if(!empty($desc)):
-                        ?>
-                        <div class="game-types__text">
-                            <?php echo $desc?>
-                        </div>
-                        <?php
-                        endif;
-                        ?>
-                    </div>
                     <?php
-                endforeach;
-                ?>
+                    endforeach;
+                    ?>
+                </div>
             </div>
         </div>
     </div>
