43998623cd240fee31e2ab1f310e7cfa83b2241c
eightalex <eight.alex@gmail.com>
2024-06-16 18:25:10 +0300

    Updated breadcrumbs

--- theme-parts/modules/breadcrumbs.php
+++ theme-parts/modules/breadcrumbs.php
@@ -8,6 +8,10 @@ $title = get_the_title();
 if(is_tax() || is_category()){
     $title = get_queried_object()->name;
 }
+
+if(is_search()) {
+    $title = __('Search results');
+}
 ?>
 
 <nav class="breadcrumbs<?php echo $inline ? ' breadcrumbs_inline' : '' ?>">
@@ -16,11 +20,12 @@ if(is_tax() || is_category()){
     <?php endif; ?>
         <ul class="breadcrumbs__inner">
             <li class="breadcrumbs__item">
-               <a href="<?php echo home_url()?>">Home</a>
+               <a href="<?php echo home_url() ?>">
+                   <?php echo __('Home'); ?>
+               </a>
             </li>
-
             <li class="breadcrumbs__item">
-                <?php echo $title?>
+                <?php echo $title ?>
             </li>
         </ul>
     <?php if (!$inline) : ?>
