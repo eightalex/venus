102a9135b8a08ebc1cf8b4714979a56c761e16c0
Ushakov-Demon <dimon.milion2@gmail.com>
2024-06-25 01:09:03 +0300

    Default Casinos loop

--- taxonomy-casino-category.php
+++ taxonomy-casino-category.php
@@ -6,23 +6,67 @@ $app_banner_txt	= carbon_get_term_meta($ID, 'app_banner_txt');
 $content_editor = carbon_get_term_meta($ID, 'content_editor');
 $content 		= carbon_get_term_meta($ID, 'ud_cat_content');
 
-$posts 	= get_posts(['category' => $ID, 'post-status' => 'publish']);
-$tags 	= apply_filters('ud_get_tax_posts_tags', $posts);
+$cas_args = [
+    'items_number'   => 9,
+	'category'       => $ID
+];
+
+$casinos = apply_filters('ud_get_casinos', $cas_args);
 
 if(empty($app_banner_img)){
 	$app_banner_img = get_stylesheet_directory_uri()."/assets/images/banner/banner.svg";
 }
 
 get_header();
-?>
-
-<?php 
 get_template_part('/theme-parts/modules/app-banner', '', ['img' => $app_banner_img, 'txt' => $app_banner_txt]);
 
-
-if($tags){
-	get_template_part("/theme-parts/modules/tags", "list", ["tags" => $tags]);
-}
+if($casinos->have_posts()):
+	?>
+	<section class="section section_suits">
+		<div class="container">
+			<div class="section__inner">
+				<div class="section__content">
+					<div class="card-list card-list_col-2">
+						<?php
+						while($casinos->have_posts()):
+							$casinos->the_post();
+	
+							$id                         = get_the_ID();
+							$cas_img_id                 = get_post_thumbnail_id();
+							$cas_img_data               = apply_filters('ud_get_file_data', $cas_img_id);
+							$desc                       = get_post_meta($id, 'casino_short_desc', true);
+							$casino_overall_rating      = floatval(get_post_meta($id, 'casino_overall_rating', true));
+							$casino_external_link       = get_post_meta($id, 'casino_external_link', true);
+	
+							$atts = [
+								'title'             => get_the_title(),
+								'img_src'           => $cas_img_id !== 0? $cas_img_data['src']: '',
+								'img_alt'           => $cas_img_id !== 0? $cas_img_data['alt']: get_the_title(),
+								'rating'            => $casino_overall_rating,
+								'desc'              => $desc,
+								'permalink'         => get_the_permalink(),
+								'external_link'     => $casino_external_link,
+							];
+							do_action('print_single_casino_template', $atts);
+						endwhile;
+						?>
+					</div>
+				</div>
+				<?php
+					$max_pages = $casinos->max_num_pages;
+					$paged = $casinos->query['paged'];
+					if($max_pages  > 1){
+						$pagenavi_items = apply_filters('my_pagination', $paged, $max_pages, "casinois-page");
+						echo "<div class='content-cards__footer'>
+									{$pagenavi_items}
+							  </div>";
+					}
+				?>
+			</div>
+		</div>
+	</section>
+<?php
+endif;
 
 if(!empty($content_editor)){
 	$part = ['text_editor' => $content_editor];
