160d22df4ebb4f9019b03368023e10a621e711f0
Ushakov-Demon <dimon.milion2@gmail.com>
2024-07-04 11:57:41 +0300

    Add Vendor to single game page

--- theme-parts/modules/banner-single_game.php
+++ theme-parts/modules/banner-single_game.php
@@ -1,17 +1,20 @@
 <?php
 extract($args);
 
-$game_img_id    = get_post_thumbnail_id($id);
-$game_img_data  = apply_filters('ud_get_file_data', $game_img_id);
-$rat            = floatval(get_post_meta($id, 'game_rating_one', true));
-$short_desc     = get_post_meta( $id, 'game_short_desc', true );
-$unit_detailed  = get_post_meta( $id, 'unit_detailed_tc', true );
-$external       = get_post_meta( $id, 'game_external_link', true );
-$button_notice  = get_post_meta( $id, 'game_button_notice', true );
-$button_title   = get_post_meta( $id, 'game_button_title', true );
-$def_btntxt     = !empty(get_option('games_play_now_title'))? get_option('games_play_now_title'): __('PLAY NOW');
-$btn_txt        = !empty($button_title)? $button_title: $def_btntxt;
-
+$game_img_id        = get_post_thumbnail_id($id);
+$game_img_data      = apply_filters('ud_get_file_data', $game_img_id);
+$rat                = floatval(get_post_meta($id, 'game_rating_one', true));
+$vendor             = wp_get_post_terms($id, 'vendor');
+$vendor_img_id      = get_term_meta($vendor[0]->term_id, 'taxonomy-image-id', true);
+$vendor_img_data    = apply_filters('ud_get_file_data', $vendor_img_id);
+$vendor_url         = get_term_link($vendor[0]->term_id);      
+$short_desc         = get_post_meta( $id, 'game_short_desc', true );
+$unit_detailed      = get_post_meta( $id, 'unit_detailed_tc', true );
+$external           = get_post_meta( $id, 'game_external_link', true );
+$button_notice      = get_post_meta( $id, 'game_button_notice', true );
+$button_title       = get_post_meta( $id, 'game_button_title', true );
+$def_btntxt         = !empty(get_option('games_play_now_title'))? get_option('games_play_now_title'): __('PLAY NOW');
+$btn_txt            = !empty($button_title)? $button_title: $def_btntxt;
 
 get_template_part('/theme-parts/modules/breadcrumbs');
 ?>
@@ -31,10 +34,19 @@ get_template_part('/theme-parts/modules/breadcrumbs');
                                     <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/icons/star.svg'?>" alt="star">
                                 </div>
                             </div>
+                            <?php
+                            if(!empty($vendor)):
+                                $img_src = !empty($vendor_img_id) && !empty($vendor_img_data)? $vendor_img_data['src']:get_stylesheet_directory_uri().'/assets/images/game.png'; 
+                            ?>
                             <div class="banner-casino__vendor">
-                                <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/game.png'?>" alt="vendor">
-                                Vendor title
+                                <a href="<?php echo $vendor_url?>">
+                                    <img src="<?php echo $img_src?>" alt="<?php echo 'vendor ' . $vendor[0]->name?>">
+                                </a>    
+                                    <?php echo $vendor[0]->name?>
                             </div>
+                            <?php
+                            endif;
+                            ?>
                         </div>
                     </div>
                     <?php
