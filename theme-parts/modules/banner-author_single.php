7f66ab2cbc2b5a1955b636513686c8d89ea874be
Igor Sikora <iseegor87@gmail.com>
2024-10-02 22:09:42 +0300

    Add PageSpeed Optimization improvements

--- theme-parts/modules/banner-author_single.php
+++ theme-parts/modules/banner-author_single.php
@@ -19,16 +19,7 @@ if($app_banner_img){
 }
 
 ?>
-
-<?php if (!empty($img_data)): ?>
-    <style>
-       .section_custom_bg::before{
-           background-image: url("<?php echo $img_data['src'] ?>");
-       }
-    </style>
-<?php endif; ?>
-
-<section class="section section_bg section_bg_7 section_custom_bg">
+<section class="section section--overlay section_bg_7 section_custom_bg">
     <div class="container">
         <div class="section__inner">
             <div class="banner-author">
@@ -72,5 +63,24 @@ if($app_banner_img){
                 </footer>
             </div>
         </div>
+    </div>
+
+	<?php
+	if ( !empty( $img_data ) ) {
+		$overlay_img_id = attachment_url_to_postid($img_data['src']);
+
+		$overlay_img = $img_data['src'];
+		$overlay_img_sm = wp_get_attachment_image_src($overlay_img_id, 'large')[0];
+
+	} else {
+		$overlay_img = get_stylesheet_directory_uri() . '/assets/images/section/bg7.webp';
+		$overlay_img_sm = get_stylesheet_directory_uri() . '/assets/images/section/bg7-sm.webp';
+	}
+	?>
+    <div class="section__overlay">
+        <picture>
+            <source media="(max-width: 1024px)" srcset="<?php echo $overlay_img_sm; ?>">
+            <img src="<?php echo $overlay_img; ?>" alt="" class="section__overlay-img" fetchpriority="high">
+        </picture>
     </div>
 </section>
