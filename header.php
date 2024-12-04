b8807a3a96c549652ca1fc73f86357c997b42606
eightalex <eight.alex@gmail.com>
2024-12-04 12:03:35 +0200

    Revert "fix(pagination): remove first page parameters"
    This reverts commit 48c9704e3b9c0222f41773dacad1b600aae9e825.

--- header.php
+++ header.php
@@ -13,16 +13,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Hind:wght@400;500;600&family=Montserrat:ital,wght@0,600;0,700;0,800;1,500&family=Ubuntu&display=swap" rel="stylesheet">
 
-    <?php
-
-    global $current_id;
-    $current_id = get_the_ID();
-
-    ?>
-
     <script>
         const php_vars = {
-            current_id: <?= $current_id ?>,
+            current_id: <?= get_the_ID() ?>,
             toc_excluded_pages: <?= json_encode(carbon_get_theme_option('toc_excluded_pages')) ?>,
             toc_excluded_posts: <?= json_encode(carbon_get_theme_option('toc_excluded_posts')) ?>,
         };
