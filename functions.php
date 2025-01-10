9a3b641e38eec61424241f33b699d2c7c295cefc
radoshDev <radosh.dev@gmail.com>
2025-01-10 18:02:29 +0200

    fix(casino-card): rename to v1 and switch code

--- functions.php
+++ functions.php
@@ -1559,8 +1559,8 @@ function ud_custon_fields() {
                     Field::make('checkbox', 'cas_show_pagination', __('Show pagination'))
                         ->set_default_value('yes')
                         ->set_width(50),
-                    Field::make('checkbox', 'casino_card_v2', __('Card version 2'))
-                        ->set_default_value('yes')
+                    Field::make('checkbox', 'casino_card_v1', __('Card version 1'))
+                        ->set_default_value('no')
                         ->set_width(50),
                     // Field::make('image', 'cas_bg', __('Background'))
                     //     ->set_value_type('url')
--- theme-parts/modules/casino-card.php
+++ theme-parts/modules/casino-card.php
@@ -9,7 +9,7 @@ $cas_args = [
 ];
 
 $show_pagination = isset($content) && isset($content['cas_show_pagination']) && $content['cas_show_pagination'];
-$casino_card_v2  = isset($content) && isset($content['casino_card_v2']) && $content['casino_card_v2'];
+$casino_card_v1  = isset($content) && isset($content['casino_card_v1']) && $content['casino_card_v1'];
 
 if(get_post_type() == 'casino'){
     $term_data = wp_get_post_terms($id, 'casino-category');
@@ -62,7 +62,42 @@ if($casinos->have_posts()):
             endif;
             ?>
             <div class="section__content">
-                <?php if($casino_card_v2): ?>
+                <?php if($casino_card_v1): ?>
+                    <table class="card-list card-list_col-2 table">
+                        <?php
+                        while($casinos->have_posts()):
+                            $casinos->the_post();
+
+                            $id                         = get_the_ID();
+                            $is_first_casino            = $casinos->current_post === 0;
+                            $cas_img_id                 = get_post_thumbnail_id();
+                            $cas_img_data               = apply_filters('ud_get_file_data', $cas_img_id);
+                            $desc                       = get_post_meta($id, 'casino_short_desc', true);
+                            $casino_overall_rating      = floatval(get_post_meta($id, 'casino_overall_rating', true));
+                            $casino_external_link       = get_post_meta($id, 'casino_external_link', true);
+
+                            if ($is_first_casino) {
+                                $float_bar_casino_id = $id;
+                            }
+
+                            $atts = [
+                                'title'             => get_the_title(),
+                                'img_src'           => $cas_img_id !== 0? $cas_img_data['src']: '',
+                                'img_alt'           => $cas_img_id !== 0? $cas_img_data['alt']: get_the_title(),
+                                'rating'            => $casino_overall_rating,
+                                'desc'              => $desc,
+                                'permalink'         => get_the_permalink(),
+                                'external_link'     => $casino_external_link,
+                                'is_table'          => true,
+                            ];
+
+                            echo '<tr>';
+                                do_action('print_single_casino_template', $atts);
+                            echo '</tr>';
+                        endwhile;
+                        ?>
+                    </table>
+                <?php else: ?>
                     <div class="card-list card-list_col-1">
                         <?php while($casinos->have_posts()): ?>
 
@@ -142,43 +177,6 @@ if($casinos->have_posts()):
                         </div>
                         <?php endwhile; ?>
                     </div>
-
-                <?php else: ?>
-
-                    <table class="card-list card-list_col-2 table">
-                        <?php
-                        while($casinos->have_posts()):
-                            $casinos->the_post();
-
-                            $id                         = get_the_ID();
-                            $is_first_casino            = $casinos->current_post === 0;
-                            $cas_img_id                 = get_post_thumbnail_id();
-                            $cas_img_data               = apply_filters('ud_get_file_data', $cas_img_id);
-                            $desc                       = get_post_meta($id, 'casino_short_desc', true);
-                            $casino_overall_rating      = floatval(get_post_meta($id, 'casino_overall_rating', true));
-                            $casino_external_link       = get_post_meta($id, 'casino_external_link', true);
-
-                            if ($is_first_casino) {
-                                $float_bar_casino_id = $id;
-                            }
-
-                            $atts = [
-                                'title'             => get_the_title(),
-                                'img_src'           => $cas_img_id !== 0? $cas_img_data['src']: '',
-                                'img_alt'           => $cas_img_id !== 0? $cas_img_data['alt']: get_the_title(),
-                                'rating'            => $casino_overall_rating,
-                                'desc'              => $desc,
-                                'permalink'         => get_the_permalink(),
-                                'external_link'     => $casino_external_link,
-                                'is_table'          => true,
-                            ];
-
-                            echo '<tr>';
-                                do_action('print_single_casino_template', $atts);
-                            echo '</tr>';
-                        endwhile;
-                        ?>
-                    </table>
 
                 <?php endif; ?>
             </div>
